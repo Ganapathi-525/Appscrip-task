cda2dd72e6192db3433392ac206971b3319df409
Bakthula Ganapathi <144654058+Ganapathi-525@users.noreply.github.com>
2024-05-20 16:20:31 +0530

    adding

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header/header';
+import Filter from './Components/filter/filter';
+import Footer from './Components/Footer/footer';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Filter/>
+    <Footer/>
+    
+
+    
+    </>
+  );
+}
+
+export default App;
